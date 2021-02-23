1bfe5e2111f1277494e9182d156c122947ddbbb9
trits9 <79425815+trits9@users.noreply.github.com>
2021-02-23 14:40:45 -0700

    Add files via upload

---
+++ accelerometer-test/accelerometer-test.ino
@@ -0,0 +1,56 @@
+#include <LiquidCrystal_I2C.h>
+#include <Arduino_LSM9DS1.h>
+#include <Wire.h> 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup()
+{
+  lcd.begin();
+  if (!IMU.begin())
+  {
+    Serial.println("Failed to initialize IMU!");
+    exit(1);
+  }
+}
+
+void loop()
+{
+  float x, y, z, delta = 0.05;
+
+  if (IMU.accelerationAvailable())
+  {
+    IMU.readAcceleration(x, y, z);
+    Serial.println(y);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Angle of Boat Lean");
+    lcd.setCursor(0,1);
+    lcd.print(abs(y*90));
+    lcd.setCursor(8,1);
+    lcd.print("Degrees");
+    
+    if(y <= delta && y >= -delta)
+      Serial.println("flat");
+      
+        
+    else if(y > delta && y < 1 - delta)
+    {
+      Serial.println("tilted to the left");
+      lcd.setCursor(0,2);
+      lcd.print("Leaning to Port");
+    }
+    else if(y >= 1 - delta)
+      Serial.println("left");
+      
+    else if(y < -delta && y > delta - 1)
+    {
+      Serial.println("tilted to the right");
+      lcd.setCursor(0,2);
+      lcd.print("Leaning to Starboard");
+    } 
+    else
+      Serial.println("right");
+  }
+
+  delay(150);
+}
