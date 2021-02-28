a86adb30fb51c3f2d9fb3eaac878cace22aa8999
juandiegovil <66028457+juandiegovil@users.noreply.github.com>
2021-02-28 17:28:48 -0600

    Update stroke_per_min.ino

--- stroke_per_min.ino
+++ stroke_per_min.ino
@@ -8,27 +8,27 @@ void setup() {
 
 //input is the value from accelerometer
 //long prev_time=millis(); //the intial time on which all times will be taken
-long interval, current_time, stroke_rate; //the difference between 2 strokes
+long interval_strokes, current_time, stroke_rate; //the difference between 2 strokes
 long impulse = 1000; //the threshold at which something will be considered a stroke
 long strokerate[3];
-long prevtime[3];
-long total = 0;
+long prevtime_strokes[3];
+long total_strokes = 0;
 
 long stroke_rate (long input) {
   //input is the value from accelerometer
   //calculation one stroke
   if (input >= impulse){
-    for (int i = 0; i < sizeof(prevtime) - 1; i++){
-      prevtime[i] = prevtime[i + 1];
+    for (int i = 0; i < sizeof(prevtime_strokes) - 1; i++){
+      prevtime_strokes[i] = prevtime_strokes[i + 1];
     }
     current_time = millis();
-    prevtime[sizeof(prevtime) - 1] = current_time; 
-    interval = prevtime[sizeof(prevtime) - 1]-prevtime[sizeof(prevtime) - 2]; 
+    prevtime_strokes[sizeof(prevtime_strokes) - 1] = current_time; 
+    interval = prevtime_strokes[sizeof(prevtime_strokes) - 1]-prevtime_strokes[sizeof(prevtime_strokes) - 2]; 
     //interval=current_time-prev_time;
-    stroke_rate = 60/(interval*1000);
+    stroke_rate = 60/(interval_strokes*1000);
     return stroke_rate;
   } 
-  return total;
+  return total_strokes;
 }
 
 
@@ -55,6 +55,6 @@ long average_stroke_rate(long input){
     //}
   }
   
-  total = (strokerate[0] + strokerate[1] + strokerate[2])/3;
+  total_strokes = (strokerate[0] + strokerate[1] + strokerate[2])/3;
   return total;
 }
