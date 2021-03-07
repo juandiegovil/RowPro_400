a17bc1e7b5743f46091eb99a360551da41298e28
juandiegovil <66028457+juandiegovil@users.noreply.github.com>
2021-03-07 13:56:29 -0600

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -7,35 +7,29 @@
 #include "Smoothing_copy.ino";
 #include "accelerometer-test.ino";
 
-//Timing variables
-float prev_time_timing;
-
-//Gyro variables
-float intervalAverage_Gyro; //the difference between 2 strokes
-float prevtime_Gyro[10];
+void readAccAndGyro();
+void printOutput();
+void displayOutput();
 
 //Smoothing variables
-float smooth_angular_velocity = 0;
-const int numReadings = 10;
+/*const int numReadings = 10;
 int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
-int total = 0; 
+int total = 0;*/ 
 
 //Strokes variables
-float interval_strokes, current_time, stroke_rate; //the difference between 2 strokes
-float impulse = 1000; //the threshold at which something will be considered a stroke
-float strokerate[3];
-float prevtime_strokes[3];
-float total_strokes = 0;
+float strokeRate, smoothStrokeRate; //the difference between 2 strokes
+float impulse = 1.0; //the threshold at which something will be considered a stroke
+float prevtimeStrokes[3];
+int strokeCount = 0;
+
+//Level variables
+int rollAngle;
+int smoothRollAngle;
 
 //Main variables
-boolean on_or_off;
+//boolean on_or_off;
 float AccX, AccY, AccZ, GyroX, GyroY, GyroZ;
-float angular_velocity;
-float smooth_angular_velocity;
-float changeInAngle;
-float accelerations;
-float smooth_accelarations;
 
 void setup() {
     Serial.begin(19200);
@@ -47,20 +41,81 @@ void setup() {
         while (1);
     }
     delay(20);
-    //timing
-    //accelorometer data
-    //gyro data
-    //Smoother
-    //stroke per min
-    //change in angle
+    
+    prevtime_strokes[0] = 6000;
+    prevtime_strokes[1] = 3000;
+    prevtime_strokes[2] = 0;
+    
+    lcd.begin();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("ROW PRO 2.0");
+    lcd.setCursor(0, 1);
+    lcd.print("Stroke Rate:");
+    lcd.setCursor(0, 2);
+    lcd.print("Stroke Count:");
+    lcd.setCursor(0, 3);
+    lcd.print("Roll Angle:");
 }
 
 void loop() {
-    on_or_off = timing(delay_input);
-    angular_velocity = run_gyrometer();
-    smooth_angular_velocity = smoother(angular_velocity);
-    changeInAngle = ChangeInAngle(smooth_angular_velocity);
-    accelarations = run_accelerometer();
-    smooth_accelarations = smoother(accelarations);
-    stroke_rate = strokeRate(smooth_accelarations);
+    readAccAndGyro();
+    
+    rollAngle = basicRoll(AccX/16384.0, AccY/16384.0, AccZ/16384.0, GyroX/8.75, GyroY/8.75, GyroZ/8.75);
+    //rollAngle = basicRoll(AccX, AccY, AccY, GyroX, GyroY, GyroZ);
+    //smoothRollAngle = smoother(rollAngle);
+    
+    strokeRate = strokeRate(AccX, impulse, prevtimeStrokes, strokeCount);
+    //smoothStrokeRate = smoother(strokeRate);
+    printOutput();
+    displayOutput();
+    //on_or_off = timing(delay_input);
+}
+
+void readAccAndGyro() {
+    if ((IMU.accelerationAvailable())&& (IMU.gyroscopeAvailable())){
+        IMU.readAcceleration(AccX, AccY, AccZ);
+        /*AccX = AccX /16384.0;
+        AccX = AccY /16384.0;
+        AccX = AccZ /16384.0;*/
+        IMU.readGyroscope(GyroX, GyroY, GyroZ);
+        /*GyroX = GyroX / 8.75;
+        GyroY = GyroY / 8.75;
+        GyroZ = GyroZ / 8.75;*/
+    }
+}
+
+void printOutput() {
+    Serial.print("Roll Angle: ");
+    Serial.print(abs(angle));
+    Serial.print("  deg");
+    Serial.print('\t\t');
+
+    Serial.print("Stroke Count:  ");
+    Serial.println(strokeCount);
+    Serial.print('\t');
+    Serial.print ("prevtimeStrokes array:   ");
+    Serial.print(prevtimeStrokes[0]);
+    Serial.print ("  ");
+    Serial.print(prevtimeStrokes[1]);
+    Serial.print ("  ");
+    Serial.print(prevtimeStrokes[2]);
+    Serial.print('\t');
+    Serial.print("Time between stroke in ms:  ");
+    Serial.print(intervalStrokes);
+    Serial.print('\t');
+    Serial.print("Stroke Rate:  ");
+    Serial.print(strokeRate);
+}
+
+void displayOutput() {
+    lcd.setCursor(16, 1);
+    lcd.print(strokeRate);
+    
+    lcd.setCursor(15, 2);
+    lcd.print(strokeCount);
+    
+    lcd.setCursor(15, 3);
+    lcd.print(rollAngle);
+
 }
